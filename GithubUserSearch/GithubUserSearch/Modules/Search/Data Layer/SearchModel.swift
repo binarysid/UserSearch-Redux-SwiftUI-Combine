cbadc19ec599ad8c9a5abf830705884df0ff5937
Linkon Sid <linkon@sheba.xyz>
2023-03-13 02:29:37 +0600

    add Model in Data Layer

---
+++ GithubUserSearch/GithubUserSearch/Modules/Search/Data Layer/SearchModel.swift
@@ -0,0 +1,33 @@
+//
+//  SearchModel.swift
+//  GithubUsers
+//
+//  Created by Linkon Sid on 12/3/23.
+//
+
+import Foundation
+
+struct GitHubResponse: Codable {
+    let totalCount: Int?
+    let incompleteResults: Bool?
+    let items: [GitHubUser]
+
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case incompleteResults = "incomplete_results"
+        case items
+    }
+}
+
+struct GitHubUser: Codable {
+    let login: String
+    let id: Int
+    let avatarUrl: String
+    let reposUrl: String
+
+    enum CodingKeys: String, CodingKey {
+        case login, id
+        case avatarUrl = "avatar_url"
+        case reposUrl = "repos_url"
+    }
+}
