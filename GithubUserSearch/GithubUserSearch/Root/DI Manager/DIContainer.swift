b72359e934c0481d04bf67723e463b2719221e90
Linkon Sid <linkon@sheba.xyz>
2023-03-13 02:45:06 +0600

    add DIContainer class and Injector property wrapper

---
+++ GithubUserSearch/GithubUserSearch/Root/DI Manager/DIContainer.swift
@@ -0,0 +1,28 @@
+
+import Foundation
+
+protocol DIContainerProtocol {
+  func register<Service>(type: Service.Type, component: Any)
+  func resolve<Service>(type: Service.Type) -> Service
+}
+
+final class DIContainer: DIContainerProtocol {
+
+  static let shared = DIContainer()
+
+  private init() {}
+
+  var services: [String: Any] = [:]
+
+  func register<Service>(type: Service.Type, component: Any) {
+      services["\(type)"] = component
+  }
+
+  func resolve<Service>(type: Service.Type) -> Service {
+      let key = "\(type)"
+      let dependency = services[key]
+      precondition(dependency != nil, "No dependency found for key \(key). Must register dependency before resolving it")
+      return dependency as! Service
+  }
+    
+}
