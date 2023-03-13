2e05775c60eee0dd8379eee3beffff6b2b5e288d
Linkon Sid <linkon@sheba.xyz>
2023-03-13 17:42:36 +0600

    Add View Model in Presentation Layer

---
+++ GithubUserSearch/GithubUserSearch/Modules/Search/Presentation Layer/Contollers/SearchViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  SearchViewModel.swift
+//  GithubUsers
+//
+//  Created by Linkon Sid on 12/3/23.
+//
+
+import Foundation
+import Combine
+
+final class SearchViewModel:SearchViewModelProtocol{
+    @Inject
+    private var searchUseCase: SearchUseCaseProtocol
+    @Published var searchQuery: String = ""
+    @MainActor @Published var data:[SearchDTO] = []
+    private var cancellables: Set<AnyCancellable> = []
+
+    init() {
+        $searchQuery
+            .debounce(for: .milliseconds(500), scheduler: DispatchQueue.main)
+            .removeDuplicates()
+            .sink { [weak self] query in
+                self?.search(query: query)
+            }
+            .store(in: &cancellables)
+    }
+
+    func search(query: String) {
+        Task {
+            let searchResults = await searchUseCase.search(by: query)
+            guard case .success(let data) = searchResults else{
+                return
+            }
+            await MainActor.run(body: {
+                self.data = data
+            })
+        }
+    }
+}
