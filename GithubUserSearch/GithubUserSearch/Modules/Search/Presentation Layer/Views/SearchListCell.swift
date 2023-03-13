6bb39bf02b2f8df67c34d8851b2d9eeeb65e5166
Linkon Sid <linkon@sheba.xyz>
2023-03-13 20:09:37 +0600

    Add SearchResult List Subview to Presentation Layer

---
+++ GithubUserSearch/GithubUserSearch/Modules/Search/Presentation Layer/Views/SearchListCell.swift
@@ -0,0 +1,31 @@
+//
+//  SearchListCell.swift
+//  GithubUsers
+//
+//  Created by Linkon Sid on 13/3/23.
+//
+
+import SwiftUI
+
+struct SearchListCell: View {
+    @State var item: SearchDTO
+    var body: some View {
+        HStack{
+            AsyncImage(url: item.avatar
+                                           , content: { image in
+                                    image.resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: 35)
+                                        .clipShape(Circle())
+                                }, placeholder: {
+                                    ProgressView()
+                                })
+            .padding(.leading,8)
+            
+                        
+            Text(item.name)
+                .padding(.leading,8)
+        }
+        .padding(16)
+    }
+}
---
+++ GithubUserSearch/GithubUserSearch/Modules/Search/Presentation Layer/Views/SearchResultView.swift
@@ -0,0 +1,17 @@
+//
+//  SearchResultView.swift
+//  GithubUsers
+//
+//  Created by Linkon Sid on 13/3/23.
+//
+
+import SwiftUI
+
+struct SearchResultsView: View {
+    var data: [SearchDTO] = []
+    var body: some View {
+        List(data) { result in
+            SearchListCell(item: result)
+        }
+    }
+}
