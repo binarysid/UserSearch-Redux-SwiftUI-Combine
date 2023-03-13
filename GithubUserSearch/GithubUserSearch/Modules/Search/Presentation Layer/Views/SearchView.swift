19633ddb290e2ec7736c716eab28da9eaecfcac2
Linkon Sid <linkon@sheba.xyz>
2023-03-13 20:11:37 +0600

    Add Search Parent view to Presentation Layer

---
+++ GithubUserSearch/GithubUserSearch/Modules/Search/Presentation Layer/Views/SearchView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  GithubUsers
+//
+//  Created by Linkon Sid on 12/3/23.
+//
+
+import SwiftUI
+
+struct SearchView<T:SearchViewModelProtocol>: View {
+    @ObservedObject var viewModel: T
+
+    var body: some View {
+        NavigationView{
+            VStack {
+                SearchBar(text: $viewModel.searchQuery)
+                    .padding(.top)
+                SearchResultsView(data: viewModel.data)
+            }
+        }
+        .navigationBarTitle(Text("Github User Search"))
+        .onDisappear{
+            viewModel.cancelSearchTask()
+        }
+    }
+        
+}
+
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
