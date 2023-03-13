f5bf55b5f914199d0f7ed5899f3baf362e357348
Linkon Sid <linkon@sheba.xyz>
2023-03-13 16:41:30 +0600

    Unit test add for Search API Data source

--- GithubUserSearch/GithubUserSearchTests/Modules/Search/Mocks/Classes/MockSearchAPIClient.swift
+++ GithubUserSearch/GithubUserSearchTests/Modules/Search/Mocks/Classes/MockSearchAPIClient.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+@testable import GithubUserSearch
+
+struct MockSearchAPIClient:SearchAPIDataSourceProtocol{
+    var dataSource:String
+    func getSearchData(by name: String) async -> Result<[GithubUserSearch.GitHubUser], GithubUserSearch.APIManager.CustomError> {
+        let testBundle = Bundle(for: GithubUserSearchTests.self)
+        guard let url =
+                testBundle.url(forResource:dataSource,withExtension:"json") else{
+            return .failure(.BadURL)
+        }
+        do
+        {
+            let data = try Data(contentsOf: url)
+            let userData = try JSONDecoder().decode(GitHubResponse.self, from: data)
+            guard userData.items.count>0 else{return .failure(.NoDataFound)}
+            return .success(userData.items)
+        }catch{
+            return .failure(.ServiceNotFound)
+        }
+    }
+    
+    
+    
+}
