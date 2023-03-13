cf0d9d1d191d49ce921234e5027f89daaed1f9eb
Linkon Sid <linkon@sheba.xyz>
2023-03-13 17:55:48 +0600

    Mock add for DTO

--- GithubUserSearch/GithubUserSearchTests/Modules/Search/Domain Layer/Mocks/Classes/MockSearchRepository.swift
+++ GithubUserSearch/GithubUserSearchTests/Modules/Search/Domain Layer/Mocks/Classes/MockSearchRepository.swift
@@ -9,11 +9,7 @@ import Foundation
 @testable import GithubUserSearch
 
 final class MockSearchRepository:SearchRepositoryProtocol{
-    let data = [SearchDTO(id: 1, name: "binarysid"),
-                SearchDTO(id: 2, name: "hharoon"),
-                SearchDTO(id: 3, name: "mayckonx")
-    ]
     func getData(by name: String) async -> Result<[GithubUserSearch.SearchDTO], GithubUserSearch.APIManager.CustomError> {
-        return .success(data)
+        return .success(SearchDTOMock)
     }
 }
---
+++ GithubUserSearch/GithubUserSearchTests/Modules/Search/Domain Layer/Mocks/Data/SearchDTOMock.swift
@@ -0,0 +1,14 @@
+//
+//  SearchDTOMock.swift
+//  GithubUserSearchTests
+//
+//  Created by Linkon Sid on 13/3/23.
+//
+
+import Foundation
+@testable import GithubUserSearch
+
+let SearchDTOMock = [SearchDTO(id: 1, name: "binarysid"),
+            SearchDTO(id: 2, name: "hharoon"),
+            SearchDTO(id: 3, name: "mayckonx")
+]
