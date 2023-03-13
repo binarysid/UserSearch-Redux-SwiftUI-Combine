f7f1451ed348df29a646da0ba25cd64c2b74739d
Linkon Sid <linkon@sheba.xyz>
2023-03-13 20:25:01 +0600

    ViewConfigurator invkoed from Main app to load the search view

--- GithubUserSearch/GithubUserSearch/Root/GithubUserSearchApp.swift
+++ GithubUserSearch/GithubUserSearch/Root/GithubUserSearchApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GithubUserSearchApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SearchViewConfigurator().configure()
         }
     }
 }
